e0e8ba97d35a23d9f6a6e3aa9e47ab2ea5e1e9a3
brianKungu <bkungu07@gmail.com>
2022-07-14 16:13:44 +0300

    contest

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        Background: "#F8F9D7",
+        Primary: "#6E85B7",
+        Secondary: "#B2C8DF",
+        Neutral: "#C4D7E0",
+      },
+    },
+  },
+  plugins: [],
+};
